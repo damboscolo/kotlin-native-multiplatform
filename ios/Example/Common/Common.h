a3b149c08a343d945fb1bd1a4ca17c0416e5cdac
Daniele Boscolo <danieleboscolo@tokenlab.com.br>
2018-04-23 15:02:32 -0300

    Configure iOS application

---
+++ ios/Example/Common/Common.h
@@ -0,0 +1,19 @@
+//
+//  Common.h
+//  Common
+//
+//  Created by Daniele Boscolo on 23/04/18.
+//  Copyright © 2018 Tokenlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Common.
+FOUNDATION_EXPORT double CommonVersionNumber;
+
+//! Project version string for Common.
+FOUNDATION_EXPORT const unsigned char CommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Common/PublicHeader.h>
+
+
